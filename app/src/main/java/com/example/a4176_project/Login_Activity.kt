4f39f21049f9174bf03be8019043b9b06bcefdf8
zhang <anran.zhang@dal.ca>
2019-11-23 20:02:15 -0400

    Updated ui design navigation not done yet

---
+++ app/src/main/java/com/example/a4176_project/Login_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.a4176_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
---
+++ app/src/main/java/com/example/a4176_project/Signup_Activity.kt
@@ -0,0 +1,13 @@
+package com.example.a4176_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a4176_project.R
+
+class Signup_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup)
+    }
+}
